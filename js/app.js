6be42e3074d12d262170dcb927a4ef0d829e41a7
guadavillarreal <guada.villarreal.gnv@gmail.com>
2024-02-10 04:21:12 -0300

    se agrego una opcion extra para crear un parrafo nuevo usando innetHTML, la opc1 funciona correctamente

--- js/app.js
+++ js/app.js
@@ -30,42 +30,92 @@ const cambiarTitulo = () => {
   tituloPrincipal.className = "display-1 text-info";
 };
 //2dA forma de trabajar pidiendo el obj desde la hoja de JS
-
+//----comento la 1ra opc
 //func en forma de flecha
-const verMas = () => {
-  console.log("Desde la funcion verMas");
-  //creo un prafo nuevo
-  //agrego el prafo en el html
-  //OPCION 1----------------------------
-  //creo un prafo nuevo
-  //creo una const donde voy a guardar el parrafo nuevo
-  //createElement crea etiquetas del html, 'p' la etiqueta que creo en este ej
-  const parrafoNuevo = document.createElement("p");
-  parrafoNuevo.innerHTML = `Tambien puede añadir oyentes de eventos para responder a interacciones del usuario,
-    como clics o pulsaciones de teclas. Es crucial tener en cuenta el rendimiento al manipular el DOM, ya que 
-    operaciones frecuentes pueden impactar en el rendimiento de la página. En resumen, el DOM proporciona una
-    interfaz poderosa para manipular la estructura y el contenido de la página web, lo que permite crear
-    interacciones dinámicas y experiencias personalizadas para los usuarios.`;
-  //agrega una clase de bootstrap , lead resalta
-  parrafoNuevo.className = "lead";
-  //muestra por consola la var creada cuando presiono el btnVerMas
-  console.log(parrafoNuevo);
-  //agrego el prafo en el html
-  //quiero buscar el nodo padre para el nuevo parrafo
-  //getElementsByClassName busca por nombre de clase- getElementsByTagName busca por nombre de etiqueta
-  const seccionDatos = document.getElementsByClassName("mt-5");
-  //console.log(seccionDatos); // muestra el array que encontre con el parametro buscado
-  //selecciona el elemento que quiero desde el array
-  console.log(seccionDatos[0]);
-  //para agregar un nuevo nodo hijo al final
-  //seccionDatos[0].appendChild(parrafoNuevo);
-  //para agregar un nuevo nodo hijo al inicio-principio osea despues del padre sera el primer hijo
-  //seccionDatos[0].prepend(parrafoNuevo);
-  //para agregar un nuevo nodo hijo en el medio-me pide que ingrese lo que deseo ingresar y antes de quien
-  seccionDatos[0].insertBefore(parrafoNuevo, btnVerMas);
-};
+// const verMas = () => {
+//   /*para probar que funciona la llamada a la funcion desde el btn
+//   console.log("Desde la funcion verMas");*/
+//   /*creo un prafo nuevo
+//   -agrego el prafo en el html*/
+//   //agrego el prafo en el html
+//   //quiero buscar el nodo padre para el nuevo parrafo
+//   //getElementsByClassName busca por nombre de clase- getElementsByTagName busca por nombre de etiqueta
+//   const seccionDatos = document.getElementsByClassName("mt-5");
+//   //----------------------OPCION 1----------------------------
+//   //creo una condicional para controlar la accion y estetica del boton
+//   //.innerText obtengo el contenido de txt del btn para compararlo
+//   if (btnVerMas.innerText === 'Ver mas...') {
+//     //creo un prafo nuevo
+//     //creo una const donde voy a guardar el parrafo nuevo
+//     //createElement crea etiquetas del html, 'p' la etiqueta que creo en este ej
+//     const parrafoNuevo = document.createElement("p");
+//     parrafoNuevo.innerHTML = `Tambien puede añadir oyentes de eventos para responder a interacciones del usuario,
+//       como clics o pulsaciones de teclas. Es crucial tener en cuenta el rendimiento al manipular el DOM, ya que
+//       operaciones frecuentes pueden impactar en el rendimiento de la página. En resumen, el DOM proporciona una
+//       interfaz poderosa para manipular la estructura y el contenido de la página web, lo que permite crear
+//       interacciones dinámicas y experiencias personalizadas para los usuarios.`;
+//     //agrega una clase de bootstrap , lead resalta
+//     parrafoNuevo.className = "lead";
+//     //muestra por consola la var creada cuando presiono el btnVerMas
+//     console.log(parrafoNuevo);
+//     //console.log(seccionDatos); // muestra el array que encontre con el parametro buscado
+//     //selecciona el elemento que quiero desde el array
+//     console.log(seccionDatos[0]);
+//     //para agregar un nuevo nodo hijo al final
+//     //seccionDatos[0].appendChild(parrafoNuevo);
+//     //para agregar un nuevo nodo hijo al inicio-principio osea despues del padre sera el primer hijo
+//     //seccionDatos[0].prepend(parrafoNuevo);
+//     //para agregar un nuevo nodo hijo en el medio-me pide que ingrese lo que deseo ingresar y antes de quien
+//     seccionDatos[0].insertBefore(parrafoNuevo, btnVerMas);
+//     /*al precionar una vez el btn deberia de cambiar y dejar de generar mas txt
+//     para lo cual con innerHTML cambio el txt del btn
+//     con className cambio el estilo del btn con una clase de bootstrap */
+//     btnVerMas.innerHTML = 'Ocultar';
+//     btnVerMas.className = 'btn btn-danger';
+//   } else {
+//     //genero un console.log para ver que funcione el else de la estructura
+//     console.log('aqui deberia borrar el parrafo');
+//     //muestro el elemento que deseo borrar para verificar que estoy seleccionando el correct
+//     console.log(seccionDatos[0].children[2]);
+//     //elimino el hijo al precionar el btn ocultar con el metodo removeChild
+//     seccionDatos[0].removeChild(seccionDatos[0].children[2]);
+//     //SIEMPRE verificar que los cambio sean los correctos asi no cree errores comofuncionar una sola vez, verificar txt para no tener errores con la estructura if xej
+//     //cambio el txt del btn
+//     btnVerMas.innerText = 'Ver mas...';
+//     //cambio el estilo para que quede esteticamente como el del inico
+//     btnVerMas.className = 'btn btn-outline-info';
+//   }
+// };
 
 //trabajo trayendo un obj desde la hoja de JS
+//Opcion 1 funciona correctamente 
+
+/*2da Opcion -utiluzo inerHTML pero no me permite controlar tan detalladamente */
+const seccionDatos = document.getElementsByClassName("mt-5");
+const verMas = () => {
+  if (btnVerMas.innerText === "Ver mas...") {
+    seccionDatos[0].innerHTML += `<p>Tambien puede añadir oyentes de eventos para responder a interacciones del usuario,
+        como clics o pulsaciones de teclas. Es crucial tener en cuenta el rendimiento al manipular el DOM, ya que 
+        operaciones frecuentes pueden impactar en el rendimiento de la página. En resumen, el DOM proporciona una
+        interfaz poderosa para manipular la estructura y el contenido de la página web, lo que permite crear
+        interacciones dinámicas y experiencias personalizadas para los usuarios.</p>`;
+
+    btnVerMas.innerHTML = "Ocultar";
+    btnVerMas.className = "btn btn-danger";
+  } else {
+    //genero un console.log para ver que funcione el else de la estructura
+    console.log("aqui deberia borrar el parrafo");
+    //muestro el elemento que deseo borrar para verificar que estoy seleccionando el correct
+    console.log(seccionDatos[0].children[2]);
+    //elimino el hijo al precionar el btn ocultar con el metodo removeChild
+    seccionDatos[0].removeChild(seccionDatos[0].children[2]);
+    //SIEMPRE verificar que los cambio sean los correctos asi no cree errores comofuncionar una sola vez, verificar txt para no tener errores con la estructura if xej
+    //cambio el txt del btn
+    btnVerMas.innerText = "Ver mas...";
+    //cambio el estilo para que quede esteticamente como el del inico
+    btnVerMas.className = "btn btn-outline-info";
+  }
+};
 const btnVerMas = document.getElementById("btnVerMas");
 
 //si quiero agregar un evento por el elemento que elijo, debo de agregar un manejador de eventos
